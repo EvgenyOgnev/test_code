ec1996dde53f7de2de2a97eb0f7103283557b332
EvgenyOgnev <110031575+EvgenyOgnev@users.noreply.github.com>
2022-11-18 09:19:11 +0300

    Add files via upload

---
+++ test_1/Program.cs
@@ -0,0 +1,15 @@
+﻿
+string [] array = { "< ","Hello","World","333","22","home","qwer","ww" };
+
+for (int i = 0; i < array.Length; i++)
+{
+    string f = array [i];
+    int j = 0;
+    if (f.Length < 4)
+    {
+        array [j] = f;
+        Console.WriteLine(array[j]);
+    }
+    j++;
+}
+
